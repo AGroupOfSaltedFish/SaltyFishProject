1677205bc2e4223dee6128d4eb2168755a2c7d43
Lee221B <984884034@qq.com>
2017-10-18 23:46:39 +0800

    CameraActivity
    CameraActivity primarily done

--- app/src/main/java/com/example/lixinye/saltyfishproject/MainActivity.java
+++ app/src/main/java/com/example/lixinye/saltyfishproject/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.lixinye.saltyfishproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        Button button0 = (Button) findViewById(R.id.button_0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
